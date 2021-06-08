a23e89efdcf87766e780229bbdc292d809d9d4ae
Gustavo <gusbuss_1@outlook.com>
2021-06-08 17:16:10 -0500

    I added all the variables for the functions from the html files

---
+++ script.js
@@ -0,0 +1,10 @@
+var cities = [];
+
+//here I have the variables in order like in the html file 
+var cityFormEl=document.querySelector("#city-chosen");
+var cityInputEl=document.querySelector("#city;");
+var searchHistoryButtonEl = document.querySelector("#search-history");
+var citySearchInputEl=document.querySelector("#searched-city")
+var weatherOutputEl=document.querySelector("#weather-output");
+var forecastDisplayTitle = document.querySelector("#forecast-display");
+var futureForecastEl = document.querySelector("#future-forecast");
