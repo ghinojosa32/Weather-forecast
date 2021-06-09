edf25fc4f3043cc7272fe4d9be4f9192703a1373
Gustavo <gusbuss_1@outlook.com>
2021-06-08 21:58:38 -0500

    i added a for submit handler to the java script

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ var citySearchInputEl=document.querySelector("#searched-city")
 var weatherOutputEl=document.querySelector("#weather-output");
 var forecastDisplayTitle = document.querySelector("#forecast-display");
 var futureForecastEl = document.querySelector("#future-forecast");
+
+//this is going to be the function for the submit button to get the forecast for searched city
+var formsubmitHandler = function(event){
+    event.preventDefault();
+    var city = cityInputEl.nodeValue.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        //used unshift to add one or more elements to the beginning of an array and returns the new length of the array 
+        cities.unshift({city});
+        cityInputEl.value = "";
+    } else{
+        alert("please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
+}
